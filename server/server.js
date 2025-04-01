b86aa37470af1fa62a028aeb1efc59ec6df9b28b
abhay <imabhaysachan@gmail.com>
2025-04-01 10:55:39 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv').config()
+const cookieParser = require('cookie-parser');
+const connectToDB = require('./db/connect')
+const cors = require('cors')
+
+const express = require('express');
+const app = express();
+connectToDB();
+
+app.use(cors({
+    origin:'http://localhost:5173',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:[
+        'Content-Type',
+        'Authorization',
+        'Cache-Control',
+        'Expires',
+        'Pragma'
+    ],
+    credentials:true
+}))
+
+app.use(cookieParser()) 
+app.use(express.json())
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT,()=>{
+    console.log("Server ruuning on port ", PORT)
+})
